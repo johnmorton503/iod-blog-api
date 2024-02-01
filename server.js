e4ceb70eeefc9e002d205c7405cd892c2ef2c56f
John Morton <johnmorton503@gmail.com>
2024-02-01 08:12:43 +0800

    display swagger

--- server.js
+++ server.js
@@ -38,12 +38,9 @@ app.use(express.static('public'));
 
 app.use(morganMiddleware);
 
-// Swagger
-if (process.env.NODE_ENV === 'development') {
-  const swaggerUi = require('swagger-ui-express');
-  const swaggerSpec = require('./swagger/swaggerSpec');
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec.default));
-}
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger/swaggerSpec');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec.default));
 
 // // Routes
 app.use("/api/users", require("./routes/userRoutes"));
